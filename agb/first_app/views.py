a1bca2b01a9ef95898e5e22e37a00d777c91d4f0
robert_mcgregor <robotmcgregor@yahoo.com>
2023-01-22 18:36:11 +0930

    BEFORE CREATING MODELS

--- agb/first_app/views.py
+++ agb/first_app/views.py
@@ -14,5 +14,6 @@ import os
 def index(request):
     # link to template tag in templates
     my_dict = {"insert_me" : "Now I am changed - Hello I am from views.py"}
+    
     return render(request, os.path.join("first_app", "index.html"), context=my_dict)
 
