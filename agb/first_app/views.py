65d82f5960be7ae78a988a235c34d232426e4d89
robert_mcgregor <robotmcgregor@yahoo.com>
2023-01-23 18:41:01 +0930

    added table from database

--- agb/first_app/views.py
+++ agb/first_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
+from first_app.models import Topic, Webpage, AccessRecord
 
 
 # Create your views here.
@@ -12,8 +13,11 @@ import os
 #     return HttpResponse('<H1>Biomass model</H1>')
 
 def index(request):
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records':webpages_list}
     # link to template tag in templates
     my_dict = {"insert_me" : "Now I am changed - Hello I am from views.py"}
     
-    return render(request, os.path.join("first_app", "index.html"), context=my_dict)
+    #return render(request, os.path.join("first_app", "index.html"), context=my_dict)
+    return render(request, os.path.join("first_app", "index.html"), context=date_dict)
 
